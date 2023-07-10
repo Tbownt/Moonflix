7fd40bbdd4d9c18afe427b4e15b6d2014d180d9f
Tbownt <98657140+Tbownt@users.noreply.github.com>
2023-07-10 13:49:15 -0400

    aplicacion terminada

---
+++ src/Trailers/TrailerMovies.jsx
@@ -0,0 +1,150 @@
+import React from "react";
+import { useState, useEffect, useContext } from "react";
+import { Context } from "../components/NavBar";
+import ReactPlayer from "react-player";
+import { Modal, Button } from "react-bootstrap";
+import axios from "axios";
+import "../Styles/Trailer.css";
+
+function TrailerMovies({ movieTitle, movie, trailer, setTrailer }) {
+  const [videoURL, setVideoURL] = useState("");
+  const [loader, setLoader] = useState(false);
+  const { toggle } = useContext(Context);
+  const width = window.innerWidth;
+  const movieOrTv = movie.title ? "movie" : "tv";
+
+  const movieTrailerCall = async () => {
+    axios
+      .get(`https://api.themoviedb.org/3/${movieOrTv}/${movie.id}`, {
+        params: {
+          api_key: "9c0fb8cdafcb9fb7175f6ec017dc27ef",
+          append_to_response: "videos",
+        },
+      })
+      .then((res) => {
+        setVideoURL(
+          `https://www.youtube.com/watch?v=${
+            res.data.videos.results[res.data.videos.results.length - 1].key
+          }`
+        );
+      })
+      .catch((e) => {
+        console.log(e);
+        setVideoURL(null);
+      });
+  };
+
+  function loaderHandler() {
+    if (videoURL === null) setLoader("Loading...");
+    setInterval(() => {
+      setLoader("Video not found, try with another Movie / Tv Show");
+    }, 3000);
+  }
+
+  useEffect(() => {
+    movieTrailerCall();
+    loaderHandler();
+  }, [videoURL]);
+  return (
+    <>
+      <section className="player">
+        {videoURL === null ? (
+          <Modal
+            show={true}
+            size={width > 341 ? "lg" : ""}
+            fullscreen={width < 342 ? true : false}
+          >
+            <div className={toggle ? "bg-dark1" : "bg-light1"}>
+              <Modal.Header closeButton onClick={() => setTrailer(false)}>
+                <div className="title-box"></div>
+                <Modal.Title className="text-center m-auto">
+                  {movieTitle}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body className="modal-container">
+                <h3
+                  className={
+                    toggle
+                      ? "text-white text-center mt-5 mb-5 fs-3"
+                      : "text-black text-center mt-5 mb-5 fs-3"
+                  }
+                >
+                  {loader}
+                </h3>
+                <h3
+                  className={
+                    toggle
+                      ? "text-white text-center mt-5 mb-5 fs-5"
+                      : "text-black text-center mt-5 mb-5 fs-5"
+                  }
+                >
+                  {movie.overview}
+                </h3>
+
+                <h3 className={toggle ? "fw-bold mt-5" : "fw-bold mt-5"}>
+                  {movie.release_date
+                    ? movie.release_date
+                    : movie.first_air_date}
+                </h3>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="dark" onClick={() => setTrailer(false)}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </div>
+          </Modal>
+        ) : (
+          <div>
+            <Modal
+              show={true}
+              size={width > 341 ? "lg" : ""}
+              fullscreen={width < 342 ? true : false}
+            >
+              <div className={toggle ? "bg-dark1" : "bg-light1"}>
+                <Modal.Header closeButton onClick={() => setTrailer(false)}>
+                  <div className="title-box"></div>
+                  <Modal.Title className="text-center m-auto">
+                    {movieTitle}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="modal-container">
+                  <ReactPlayer
+                    url={videoURL}
+                    controls={true}
+                    playing={true}
+                    width={1000}
+                    height={width > 991 ? 600 : 300}
+                  />
+
+                  <h3
+                    className={
+                      toggle
+                        ? "text-white text-center mt-5 fs-5"
+                        : "text-black text-center mt-5 fs-5"
+                    }
+                  >
+                    {movie.overview}
+                  </h3>
+
+                  <h3 className={toggle ? "fw-bold mt-5" : "fw-bold mt-5"}>
+                    {movie.release_date
+                      ? movie.release_date
+                      : movie.first_air_date}
+                  </h3>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="dark" onClick={() => setTrailer(false)}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </div>
+            </Modal>
+          </div>
+        )}
+      </section>
+    </>
+  );
+}
+
+export default TrailerMovies;
---
+++ src/components/Movies.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState, useContext } from "react";
+import { Context } from "./NavBar";
+import { AiFillPlayCircle } from "react-icons/ai";
+import NoImg from "./assets/no_img.jpg";
+import "../Styles/Videos.css";
+import TrailerMovies from "../Trailers/TrailerMovies";
+
+function Movies() {
+  const { toggle, inputValue } = useContext(Context);
+  const input = inputValue;
+  const api = input ? "search" : "discover";
+  // const [page, setPage] = useState(1)
+  const [moviesData, setMoviesData] = useState([]);
+  const [trailer, setTrailer] = useState(false);
+  const [movieTitle, setMovieTitle] = useState("");
+  const [movie, setMovie] = useState({});
+
+  const movieCall = async () => {
+    const data = await axios.get(`https://api.themoviedb.org/3/${api}/movie`, {
+      params: {
+        api_key: "9c0fb8cdafcb9fb7175f6ec017dc27ef",
+        query: input,
+      },
+    });
+    const results = data.data.results;
+    setMoviesData(results);
+  };
+
+  useEffect(() => {
+    movieCall();
+  }, [input]);
+
+  const clickHandlder = (movie) => {
+    setMovieTitle(movie.title);
+    setMovie(movie);
+    setTrailer(!trailer);
+  };
+
+  if (moviesData.length === 0)
+    return (
+      <main className={toggle ? "mainBgColor" : "secondaryBgColor"}>
+        <section className="movies-container">
+          <h3 className={toggle ? "dark-theme" : "light-theme"}>
+            No movies found for "{input}"
+          </h3>
+        </section>
+      </main>
+    );
+  return (
+    <main className={toggle ? "mainBgColor" : "secondaryBgColor"}>
+      <section className="movies-container">
+        {moviesData.map((movie, i) => (
+          <div className={trailer ? "no-container" : "main-container"} key={i}>
+            <div className="ms-3 me-3 my-3">
+              <div
+                className="img-container"
+                onClick={() => clickHandlder(movie)}
+              >
+                <img
+                  src={
+                    movie.poster_path
+                      ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                      : NoImg
+                  }
+                  alt={i}
+                />
+                <AiFillPlayCircle
+                  color="#fff"
+                  fontSize={40}
+                  className="playIcon"
+                />
+              </div>
+            </div>
+            <h3 className={toggle ? "dark-theme" : "light-theme"}>
+              {movie.title}
+            </h3>
+          </div>
+        ))}
+        {trailer ? (
+          <TrailerMovies
+            movieTitle={movieTitle}
+            movie={movie}
+            trailer={trailer}
+            setTrailer={setTrailer}
+          />
+        ) : (
+          ""
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default Movies;
